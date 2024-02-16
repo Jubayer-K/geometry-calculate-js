ea3b66a94c707ff15f9f5cadeb88ba0df3b1a15b
Jubayer-K <jubayerkhan21@gmail.com>
2024-02-17 01:48:58 +0600

    added rectangle calculation

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-function triangleCalculate(){
-    const baseInput = document.getElementById('base');
-    const baseValue = baseInput.value;
-    const base = parseFloat(baseValue);
-    const hightInput = document.getElementById('hight');
-    const hightValue = hightInput.value;
-    const hight = parseFloat(hightValue);
-    const result = 0.5 * base * hight;
-    if(result !== 'number'){
-        return 'invalid input';
-    }else{
-        return result;
+document.getElementById("base").addEventListener("keyup", function (event) {
+    const text = event.target.value;
+    const calculateBtn = document.getElementById("btn-calculate");
+    if (text !== "") {
+      calculateBtn.removeAttribute("disabled", true);
     }
+  });
+
+function triangleCalculate() {
+  const baseInput = document.getElementById("base");
+  const baseValue = baseInput.value;
+  const base = parseFloat(baseValue);
+  const hightInput = document.getElementById("hight");
+  const hightValue = hightInput.value;
+  const hight = parseFloat(hightValue);
+  const result = 0.5 * base * hight;
+  const resultOutput = document.getElementById("final-output");
+
+  return (resultOutput.innerText = result+' cm²');
+
 }
+
+
+function rectangleCalculate(){
+    const widthInput = document.getElementById("width");
+    const widthValue = widthInput.value;
+    const width = parseFloat(widthValue);
+    const lengthInput = document.getElementById("length");
+    const lengthValue = lengthInput.value;
+    const length = parseFloat(lengthValue);
+    const result = width * length;
+    const resultOutput = document.getElementById("rect-output");
+  
+    return (resultOutput.innerText = result+' cm²');
+  
+  }
+
